13cf20c4f7502c0975de4bf9d93774fb7caf223d
Jørgen S. Dokken <dokken@simula.no>
2023-04-28 17:11:33 +0200

    Rewrite precommit

--- parse_yaml.py
+++ parse_yaml.py
@@ -1,24 +1,47 @@
 
 
 import json 
-from IPython import embed
+import argparse
 
 
-keys = ["outputs_hidden", "source_hidden"]
-with open('test_notebook.ipynb', 'r') as f:
-    data = json.load(f)
-    for cell in data["cells"]:
-        metadata = cell["metadata"]
-        for key in keys:
-            if "tags" in metadata.keys():
-                if key in metadata["tags"]:
-                    if "jupyter" in metadata.keys():
-                        metadata["jupyter"][key] = True 
-                    else:
-                        metadata["jupyter"] = {key: True}
-with open('test_notebook.ipynb', 'w') as f:
-    json.dump(data, f)
-            # data['id'] = 134 # <--- add `id` value.
-    # f.seek(0)        # <--- should reset file position to the beginning.
-    # json.dump(data, f, indent=4)
-    # f.truncate()     # remove remaining part
+def format_notebook(filename:str)->int
+    keys = ["outputs_hidden", "source_hidden"]
+    retv = 0
+    with open(filename, 'r') as f:
+        data = json.load(f)
+        for cell in data["cells"]:
+            metadata = cell["metadata"]
+            for key in keys:
+                if "tags" in metadata.keys():
+                    if key in metadata["tags"]:
+                        if "jupyter" in metadata.keys():
+                            if key in metadata["jupyter"].keys():
+                                if metadata["jupyter"][key] == False: 
+                                    metadata["jupyter"][key] = True 
+                                    retv = 1
+                            else:
+                                metadata["jupyter"][key] = True
+                                retv = 1
+                        else:
+                            metadata["jupyter"] = {key: True}
+                            retv = 1
+    if retv == 1:
+        open(filename, 'w') as f:
+            json.dump(data, f)
+    return retv
+
+
+
+def main(argv: Sequence[str] | None = None) -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filenames', nargs='*')
+    args = parser.parse_args(argv)
+
+    retv = 0
+    for filename in args.filenames:
+        retv |= format_notebook(filename)
+    return retv
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
